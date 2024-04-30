1aff35592486ca5afce492f0250b90dd1ccc5d41
kenVis1529 <tathuctrungkien2003@gmail.com>
2024-04-30 10:48:55 +0700

    Update app icon

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 export default function Home() {
     return (
         <main className="flex h-screen items-center justify-center flex-col gap-y-4">
-            <h1 className="font-bold text-3xl">Demo mongoDB with member's data ❤️</h1>
+            <h1 className="font-bold text-3xl">Demo mongoDB with member&apos;s data ❤️</h1>
             <Button size={"lg"} asChild>
-                <Link href="/member">Let's start</Link>
+                <Link href="/member">Let&apos;s start</Link>
             </Button>
         </main>
     );
